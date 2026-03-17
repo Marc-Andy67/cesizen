5df4b885749f12193ea76471eb324a8cd59812e7
Marc-Andy67 <marcandy.p@hotmail.com>
2026-03-17 23:03:52 +0100

    fix cs-fixer spacing in object-manager.php

--- tests/object-manager.php
+++ tests/object-manager.php
@@ -2,7 +2,7 @@
 
 use App\Kernel;
 
-require __DIR__ . '/bootstrap.php';
+require __DIR__.'/bootstrap.php';
 
 $kernel = new Kernel($_SERVER['APP_ENV'] ?? 'test', (bool) ($_SERVER['APP_DEBUG'] ?? true));
 $kernel->boot();

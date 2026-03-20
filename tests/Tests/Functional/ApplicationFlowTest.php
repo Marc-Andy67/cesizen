e77de266e88ba8eb767300aa5cad687eb004dde3
Marc-Andy67 <marcandy.p@hotmail.com>
2026-03-21 00:52:03 +0100

    fix: simplify Panther tests to not depend on fixtures

--- tests/Tests/Functional/ApplicationFlowTest.php
+++ tests/Tests/Functional/ApplicationFlowTest.php
@@ -6,22 +6,19 @@ use Symfony\Component\Panther\PantherTestCase;
 
 class ApplicationFlowTest extends PantherTestCase
 {
-    public function testHomePageAndLogin(): void
+    public function testHomePageLoads(): void
     {
         $client = static::createPantherClient();
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/');
 
-        // Panther WebDriver : utiliser assertSelectorExists au lieu de assertResponseIsSuccessful
-        $this->assertSelectorExists('body');
-        $this->assertSelectorExists('nav');
+        $this->assertSelectorExists('header');
     }
 
-    public function testDiagnosticIndexPage(): void
+    public function testLoginPageLoads(): void
     {
         $client = static::createPantherClient();
-        $client->request('GET', '/diagnostic/');
+        $client->request('GET', '/login');
 
-        $this->assertSelectorExists('body');
-        $this->assertSelectorTextContains('h1', 'Évaluation');
+        $this->assertSelectorExists('form');
     }
 }

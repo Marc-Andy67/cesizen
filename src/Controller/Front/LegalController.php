6e92aecec1128d463491e7403447e005fda78613
Marc-Andy67 <marcandy.p@hotmail.com>
2026-08-28 01:56:44 +0200

    fix: correct class name from AbstractControllers to AbstractController in LegalController

--- src/Controller/Front/LegalController.php
+++ src/Controller/Front/LegalController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class LegalController extends AbstractControllers
+class LegalController extends AbstractController
 {
     #[Route('/mentions-legales', name: 'app_legal_mentions')]
     public function mentionsLegales(): Response
